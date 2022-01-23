0ef89b47b0f41cac849d2f11274eb6a0ee916731
Zac Friggstad <zac.friggstad@gmail.com>
2022-01-23 08:38:25 -0700

    minor reorganization

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ Compile with "make"
 Run with
 ./nurikabe <filename> [--step]
 
-The filename is the .txt file encoding an instance (see the examples included).
+The filename is the .txt file encoding an instance, see the examples included
+in the puzzles directory.
+
 The argument --step is optional and will cause the program to wait for
-the user to enter "return" after each new character is added to the solution.
+the user to hit "return" after each new character is added to the solution.
+
+The sample puzzles are from the Nurikabe app by Conceptis.
+
+The first line of the puzzle format gives # rows and # columns.
+Then a series of lines follow, each describing one numbered square by
+giving its row index, column index, and then the value in the square.
+Top row is 0, leftmost column is 0.
--- nurikabe.cpp
+++ nurikabe.cpp
@@ -1,8 +1,14 @@
+/*
+  Nurikabe solver.
+  Zachary Friggstad, 2020
+*/
+
 #include <bits/stdc++.h>
 
 #define EMPTY 0
 #define BLACK -1
 #define DOT -2
+#define UF_PAD 8
 
 using namespace std;
 
@@ -22,17 +28,20 @@ vvi grid;
 vector<vector<vector<pii>>> order;
 
 inline int pack(int i, int j) {
-  return (i<<16)|j;
+  return (i<<UF_PAD)|j;
 }
 
+// union-find data structure for grid squares (i,j)
+// also tracks the size of each component
+// uses path compression only
 struct UnionFind {
   UnionFind(int R = 1, int C = 1) {
+    assert(C < (1<<UF_PAD));
+
     this->R = R;
     this->C = C;
-    uf.resize(R*(1<<16));
+    uf.resize(R*(1<<UF_PAD));
     sz = uf;
-    // uf.resize(R*C);
-    // sz.resize(R*C);
     reset();
   }
 
@@ -70,7 +79,9 @@ struct UnionFind {
     return sz[find(x)];
   }
 
-  int cnt(int i, int j) { return cnt(pack(i, j)); }
+  int cnt(int i, int j) {
+    return cnt(pack(i, j));
+  }
 
   int R, C;
   vector<int> uf, sz;
@@ -78,8 +89,11 @@ struct UnionFind {
 
 UnionFind uf;
 
+// print the board, highlight cell (ii, jj) if it lies on the board
 void dump(const vvi& g, int ii = -2, int jj = -2) {
-  string special = "$%&*!?";
+  // for values > 10, use a special chacter
+  string special = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+
   vector<int> vals;
 
   for (int i = 0; i < R; ++i) {
@@ -94,7 +108,13 @@ void dump(const vvi& g, int ii = -2, int jj = -2) {
         else if (x == DOT) cout << '.';
         else if (x == EMPTY) cout << '_';
         else if (x >= 10) {
-          assert(vals.size() < special.size());
+
+          // whoops, too many values > 10 on the board!
+          if (vals.size() >= special.size()) {
+            cerr << "Too many values > 10 in the input grid, cannot display" << endl;
+            assert(vals.size() < special.size());
+          }
+
           cout << special[vals.size()];
           vals.push_back(x);
         }
@@ -119,16 +139,19 @@ bool valid(int i, int j) {
 
 int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
 
+// Helper function to see how large the potential region is from a given
+// numbered cell.
+// rep is the initial numbered cell (packed to a single int)
+// adj[q] is the set of grid values that have q adjacent to their current region
 int dfs_expand(int r, int c, int rep, const vvi& g, map<int, set<int>>& adj, set<int>& seen) {
   if (!valid(r, c)) return 0;
   int p = pack(r, c);
   if (seen.find(p) != seen.end()) return 0;
   seen.insert(p);
-  // cout << "Trying: " << r << ' ' << c << endl;
+
   if (g[r][c] == BLACK) return 0;
-  if (adj[p].size() >= 2) return 0;
-  if (adj[p].size() == 1 && *adj[p].begin() != rep) return 0;
-  // cout << "Success!" << endl;
+  if (adj[p].size() >= 2) return 0; // could not put . here, would merge 2 regions
+  if (adj[p].size() == 1 && *adj[p].begin() != rep) return 0; // adjacent to a different region already
 
   int tot = 1;
   for (int k = 0; k < 4; ++k) tot += dfs_expand(r+dr[k], c+dc[k], rep, g, adj, seen);
@@ -136,10 +159,8 @@ int dfs_expand(int r, int c, int rep, const vvi& g, map<int, set<int>>& adj, set
   return tot;
 }
 
+// check if the grid is consistent so far
 bool consistent(const vvi& g) {
-  // cout << "Checking:" << endl;
-  // dump(g);
-
   // CHECK: 2x2 squares
   for (int i = 1; i < R; ++i)
     for (int j = 1; j < C; ++j)
@@ -172,7 +193,7 @@ bool consistent(const vvi& g) {
       }
 
 
-  // CHECK: numbered regions have ok size (not too big yet)
+  // CHECK: dotted regions have ok size (not too big yet)
   // and each has <= 1 given number
   uf.reset();
   for (int i = 0; i < R; ++i)
@@ -189,7 +210,6 @@ bool consistent(const vvi& g) {
         int rep = uf.find(i, j);
         if (numbered_regions.find(rep) != numbered_regions.end()) return false;
         numbered_regions.insert(rep);
-        // if (g[i][j] == 3) cout << uf.cnt(rep) << endl;
         if (uf.cnt(rep) > g[i][j]) return false;
       }
 
@@ -217,9 +237,8 @@ bool consistent(const vvi& g) {
     }
 
   // CHECK: No dotted regions without numbers are entirely closed off
-  //
 
-  // REPLACE WITH BFS THAT TERMINATES EARLY IF LARGE ENOUGH
+  // TODO (OPTIMIZATION): REPLACE WITH BFS THAT TERMINATES EARLY IF LARGE ENOUGH
   for (int i = 0; i < R; ++i)
     for (int j = 0; j < C; ++j)
       if (g[i][j] >= 1) {
@@ -234,8 +253,9 @@ bool consistent(const vvi& g) {
       }
 
 
-  // TODO:
-  // Ensure all dotted regions without numbers can reach a numbered region with appropriate size
+  // TODO - PRIORITY:
+  // Ensure all dotted regions without numbers can reach a numbered
+  // region with appropriate size
 
   return true;
 }
@@ -243,12 +263,12 @@ bool consistent(const vvi& g) {
 
 // returns 0 if a depth "max_depth" search discovers it is not solvable
 // 1 otherwise, will return as much as could be filled out with the given depth in this case
+// pi,pj is the "centre" of the search, meaning we scan grid cells nearest to (pi,pj) first
 int iterate(vvi& g, int pi, int pj, int max_depth, bool report = true) {
-  // if (!consistent(g)) return 0;
-
   if (max_depth == 0) return consistent(g);
 
   vvi orig = g;
+
   for (int depth = 1; depth <= max_depth; ++depth) {
     bool progress = false;
     for (const pii& cell : order[pi][pj]) {
@@ -257,30 +277,37 @@ int iterate(vvi& g, int pi, int pj, int max_depth, bool report = true) {
 
       // if (report && depth >= 2) cout << "Trying " << i << ' ' << j << " at depth " << depth << endl;
 
+      // try putting a . at i,j first
       vvi tmp = g;
       tmp[i][j] = DOT;
       int dot_ret = iterate(tmp, i, j, depth-1, false);
 
       if (dot_ret == 0) {
-        // if . lead to contradiction, set it to #
+        // if . lead to contradiction, set it to # and note we made progress
         g[i][j] = BLACK;
         progress = true;
       }
       else {
+        // if . did not lead to contradiction, try #
         tmp = g;
         tmp[i][j] = BLACK;
         int black_ret = iterate(tmp, i, j, depth-1, false);
 
+        // if this led to a contradiction, fix . there and note progress
         if (black_ret == 0) {
           g[i][j] = DOT;
           progress = true;
         }
       }
 
+      // if we made progress by fixing a mark
       if (progress) {
+
+        // if that fixing yields an immediate contradiction, the input
+        // board state is not solvable!
         if (!consistent(g)) {
-        g = orig;
-        return 0;
+          g = orig;
+          return 0;
         }
 
         if (report) {
@@ -289,18 +316,22 @@ int iterate(vvi& g, int pi, int pj, int max_depth, bool report = true) {
           if (step) cin.get();
         }
 
+        // this was the new change
         pi = i;
         pj = j;
         break;
       }
     }
+
+    // if we fixed a mark, then resume our search but from depth 1 again
+    // (outer for loop will increase the depth to 1)
     if (progress) depth = 0;
-    // else if (report) cout << "Increasing depth to " << depth+1 << endl;
   }
 
   return 1;
 }
 
+// check if the board g is fully solved
 bool solved(const vvi& g) {
   if (!consistent(g)) return false;
   for (int i = 0; i < R; ++i)
@@ -329,7 +360,6 @@ int main(int argc, char *argv[]) {
   }
 
   assert(fin >> R >> C);
-  assert(C < (1<<16));
 
   uf = UnionFind(R, C);
 
@@ -350,6 +380,10 @@ int main(int argc, char *argv[]) {
 
   order.resize(R, vector<vector<pii>>(C));
 
+  // precompute for every grid square (i,j)
+  // an ordering of the other grid squares by Manhattan distance
+  // Idea: when we successfully place a mark then it is likely we
+  // can place another mark nearby so start searching there.
   for (int i = 0; i < R; ++i)
     for (int j = 0; j < C; ++j) {
       vector<pair<int, pii>> ord;
@@ -360,7 +394,7 @@ int main(int argc, char *argv[]) {
       for (const auto& p : ord) order[i][j].push_back(p.second);
     }
 
-
+  // let's solve this!
   int ret = iterate(curr, 0, 0, R*C+1);
 
   if (!ret) cout << "No Solution!" << endl;
